a4ffb9c135798b8387b3c91dc25aa054e4a038a0
andysumi <andysumi@ruby.net.tokai-u.jp>
2020-04-28 17:18:55 +0900

    createIssue()のテストを追加

--- test/TestRunner.js
+++ test/TestRunner.js
@@ -11,6 +11,7 @@ function TestRunner() { // eslint-disable-line no-unused-vars
   try {
     /***** Test cases ******************************/
     testGetSpecificIssue(test, common);
+    testCreateIssue(test, common);
     /***********************************************/
   } catch (err) {
     test('Exception occurred', function f(assert) {
@@ -38,5 +39,22 @@ function testGetSpecificIssue(test, common) {
     t.equal(result.status, 200, 'issueが取得できること');
     t.equal(JSON.parse(result.body).number, no, '"number"が正しいこと');
     t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, no), '"url"が正しいこと');
+  });
+}
+
+function testCreateIssue(test, common) {
+  var client = common.getClient();
+  var assign = common.owner;
+  var label = 'bug';
+
+  test('createIssue() - 正常系', function (t) {
+    var result = client.createIssue('test', 'This is test issue.', {
+      assignees: [assign],
+      labels   : [label]
+    });
+    t.equal(result.status, 201, 'issueが作成できること');
+    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).assignees[0].login, assign, '"asign"が正しいこと');
+    t.equal(JSON.parse(result.body).labels[0].name, label, '"label"が正しいこと');
   });
 }
