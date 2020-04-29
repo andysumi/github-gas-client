8689b93f49d5f2d1161e15f5cb8700fe7a6bb39c
andysumi <andysumi@ruby.net.tokai-u.jp>
2020-04-29 09:46:14 +0900

    editIssue()のテストを追加

--- test/TestCommon.js
+++ test/TestCommon.js
@@ -1,11 +1,12 @@
-var TestCommon = function() {
+var TestCommon = function () {
   var properties = PropertiesService.getUserProperties();
   this.token = properties.getProperty('TOKEN');
   this.owner = properties.getProperty('OWNER');
   this.repo = properties.getProperty('REPOSITORY');
+  this.no = 1;
 };
 
-TestCommon.prototype.getClient = function() {
+TestCommon.prototype.getClient = function () {
   this.client = new GithubClient(this.token, this.owner, this.repo);
   return this.client;
 };
--- test/TestRunner.js
+++ test/TestRunner.js
@@ -12,6 +12,7 @@ function TestRunner() { // eslint-disable-line no-unused-vars
     /***** Test cases ******************************/
     testGetSpecificIssue(test, common);
     testCreateIssue(test, common);
+    testEditIssue(test, common);
     /***********************************************/
   } catch (err) {
     test('Exception occurred', function f(assert) {
@@ -32,7 +33,7 @@ function TestRunner() { // eslint-disable-line no-unused-vars
 
 function testGetSpecificIssue(test, common) {
   var client = common.getClient();
-  var no = 1;
+  var no = common.no;
 
   test('getSpecificIssue() - 正常系', function (t) {
     var result = client.getSpecificIssue(no);
@@ -44,17 +45,43 @@ function testGetSpecificIssue(test, common) {
 
 function testCreateIssue(test, common) {
   var client = common.getClient();
-  var assign = common.owner;
-  var label = 'bug';
+  var title = 'test';
+  var body = 'This is test issue.';
 
   test('createIssue() - 正常系', function (t) {
-    var result = client.createIssue('test', 'This is test issue.', {
-      assignees: [assign],
-      labels   : [label]
-    });
+    var result = client.createIssue(title, body);
     t.equal(result.status, 201, 'issueが作成できること');
     t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).title, title, '"title"が正しいこと');
+    t.equal(JSON.parse(result.body).body, body, '"body"が正しいこと');
+
+    common.no = JSON.parse(result.body).number;
+  });
+}
+
+function testEditIssue(test, common) {
+  var client = common.getClient();
+  var no = common.no;
+  var title = 'test - edit';
+  var body = 'This is test issue.\nEdited this issue.';
+  var assign = common.owner;
+  var label = 'bug';
+  var state = 'closed';
+
+  test('editIssue() - 正常系', function (t) {
+    var result = client.editIssue(no, {
+      title: title,
+      body: body,
+      assignees: [assign],
+      labels: [label],
+      state: state
+    });
+    t.equal(result.status, 200, 'issueが編集できること');
+    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).title, title, '"title"が正しいこと');
+    t.equal(JSON.parse(result.body).body, body, '"body"が正しいこと');
     t.equal(JSON.parse(result.body).assignees[0].login, assign, '"asign"が正しいこと');
     t.equal(JSON.parse(result.body).labels[0].name, label, '"label"が正しいこと');
+    t.equal(JSON.parse(result.body).state, state, '"state"が正しいこと');
   });
 }
