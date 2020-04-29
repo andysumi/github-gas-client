939f2d40b642f45fbaa4f648cda44ebf68402a43
andysumi <andysumi@ruby.net.tokai-u.jp>
2020-04-29 16:22:04 +0900

    getOrgRepositories()のテストを追加

--- test/TestCommon.js
+++ test/TestCommon.js
@@ -1,12 +1,18 @@
 var TestCommon = function () {
   var properties = PropertiesService.getUserProperties();
   this.token = properties.getProperty('TOKEN');
-  this.owner = properties.getProperty('OWNER');
+  this.user = properties.getProperty('USER');
+  this.org = properties.getProperty('ORGANIZATION');
   this.repo = properties.getProperty('REPOSITORY');
   this.no = 1;
 };
 
-TestCommon.prototype.getClient = function () {
-  this.client = new GithubClient(this.token, this.owner, this.repo);
+TestCommon.prototype.getClientUser = function () {
+  this.client = new GithubClient(this.token, this.user, this.repo);
+  return this.client;
+};
+
+TestCommon.prototype.getClientOrg = function () {
+  this.client = new GithubClient(this.token, this.org);
   return this.client;
 };
--- test/TestRunner.js
+++ test/TestRunner.js
@@ -14,6 +14,7 @@ function TestRunner() { // eslint-disable-line no-unused-vars
     testCreateIssue(test, common);
     testEditIssue(test, common);
     testGetUserRepositories(test, common);
+    testGetOrgRepositories(test, common);
     /***********************************************/
   } catch (err) {
     test('Exception occurred', function f(assert) {
@@ -33,26 +34,26 @@ function TestRunner() { // eslint-disable-line no-unused-vars
 }
 
 function testGetSpecificIssue(test, common) {
-  var client = common.getClient();
+  var client = common.getClientUser();
   var no = common.no;
 
   test('getSpecificIssue() - 正常系', function (t) {
     var result = client.getSpecificIssue(no);
     t.equal(result.status, 200, 'issueが取得できること');
     t.equal(JSON.parse(result.body).number, no, '"number"が正しいこと');
-    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, no), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.user, common.repo, no), '"url"が正しいこと');
   });
 }
 
 function testCreateIssue(test, common) {
-  var client = common.getClient();
+  var client = common.getClientUser();
   var title = 'test';
   var body = 'This is test issue.';
 
   test('createIssue() - 正常系', function (t) {
     var result = client.createIssue(title, body);
     t.equal(result.status, 201, 'issueが作成できること');
-    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.user, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
     t.equal(JSON.parse(result.body).title, title, '"title"が正しいこと');
     t.equal(JSON.parse(result.body).body, body, '"body"が正しいこと');
 
@@ -61,11 +62,11 @@ function testCreateIssue(test, common) {
 }
 
 function testEditIssue(test, common) {
-  var client = common.getClient();
+  var client = common.getClientUser();
   var no = common.no;
   var title = 'test - edit';
   var body = 'This is test issue.\nEdited this issue.';
-  var assign = common.owner;
+  var assign = common.user;
   var label = 'bug';
   var state = 'closed';
 
@@ -78,7 +79,7 @@ function testEditIssue(test, common) {
       state: state
     });
     t.equal(result.status, 200, 'issueが編集できること');
-    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.owner, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
+    t.equal(JSON.parse(result.body).url, Utilities.formatString('https://api.github.com/repos/%s/%s/issues/%s', common.user, common.repo, JSON.parse(result.body).number), '"url"が正しいこと');
     t.equal(JSON.parse(result.body).title, title, '"title"が正しいこと');
     t.equal(JSON.parse(result.body).body, body, '"body"が正しいこと');
     t.equal(JSON.parse(result.body).assignees[0].login, assign, '"asign"が正しいこと');
@@ -88,11 +89,21 @@ function testEditIssue(test, common) {
 }
 
 function testGetUserRepositories(test, common) {
-  var client = common.getClient();
+  var client = common.getClientUser();
 
   test('getUserRepositories() - 正常系', function (t) {
     var result = client.getUserRepositories();
     t.equal(result.status, 200, 'repositoryが取得できること');
-    t.equal(JSON.parse(result.body)[0].owner.login, common.owner, '"owner"が正しいこと');
+    t.equal(JSON.parse(result.body)[0].owner.login, common.user, '"owner"が正しいこと');
+  });
+}
+
+function testGetOrgRepositories(test, common) {
+  var client = common.getClientOrg();
+
+  test('getOrgRepositories() - 正常系', function (t) {
+    var result = client.getOrgRepositories();
+    t.equal(result.status, 200, 'repositoryが取得できること');
+    t.equal(JSON.parse(result.body)[0].owner.login, common.org, '"owner"が正しいこと');
   });
 }
